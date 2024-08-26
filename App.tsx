6fd74ad20669de1bdd6b2cb97e06aa8a6f96d30c
SammyBits <samuelbeato7@gmail.com>
2024-08-26 08:25:40 -0400

    feat: Update App and Main components
    The App component has been updated to use the SafeAreaProvider from the react-native-safe-area-context package. This ensures that the content is properly displayed within the safe area of the device screen. The StatusBar component has also been updated to use the style prop instead of the barStyle prop.
    
    In the Main component, the View component has been wrapped in a SafeAreaView component from the react-native-safe-area-context package. This ensures that the content is properly displayed within the safe area of the device screen. The useSafeAreaInsets hook has been added to calculate the safe area insets and apply them to the View component's padding.
    
    These changes improve the overall layout and ensure that the content is displayed correctly on different devices and screen sizes.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { View, StatusBar } from "react-native";
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import React from "react";
-import Main from "./components/Main";
+import { Main } from "./components/Main";
 
 export default function App() {
   return (
-    <View>
-      <StatusBar barStyle={"dark-content"} />
-      <Main />
-    </View>
+    <SafeAreaProvider>
+      <View className="bg-[#171717] flex-1 items-center justify-center px-3">
+        <StatusBar style={"auto"} />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
